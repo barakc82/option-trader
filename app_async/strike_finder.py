7bb99fb9c47c6454e439b720f0ccd7e711e6345e
Barak <barakc82@gmail.com>
2026-05-01 16:46:27 +0300

    Remove redundent abs call and redundent test

--- app_async/strike_finder.py
+++ app_async/strike_finder.py
@@ -87,12 +87,11 @@ class StrikeFinder:
             logger.error(f"No {right} option candidate found")
             return None
 
-        final_delta_signed = get_delta(current_candidate.ticker)
-        if final_delta_signed is None:
+        final_delta = get_delta(current_candidate.ticker)
+        if final_delta is None:
             logger.error(f"No delta data available for the candidate {right} option")
             return None
             
-        final_delta = abs(final_delta_signed)
         if final_delta > target_delta:
             logger.error(f"Selected {right} option {get_option_name(current_candidate)} delta {final_delta:.3f} > target {target_delta:.3f}")
             return None
