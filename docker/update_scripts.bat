ad22d82d90021dfe1787ebe9911227a27e99442d
Barak <barakc82@gmail.com>
2026-05-13 14:38:52 +0300

    Add update_scripts.bat and update gitignore

---
+++ docker/update_scripts.bat
@@ -0,0 +1,22 @@
+@echo off
+set VM_NAME=medium-sc
+
+echo Creating archive of python scripts...
+tar --exclude="__pycache__" -czf scripts.tar.gz app app_async utilities
+
+echo Sending archive to VM...
+call gcloud compute scp scripts.tar.gz %VM_NAME%:/home/barakc82/
+
+if %ERRORLEVEL% neq 0 (
+    echo Error occurred when sending files to %VM_NAME%! Exiting...
+    if exist scripts.tar.gz del scripts.tar.gz
+    exit /b %ERRORLEVEL%
+)
+
+echo Extracting and updating scripts in Docker container...
+gcloud compute ssh %VM_NAME% --command "tar -xzf /home/barakc82/scripts.tar.gz -C /home/barakc82/ && sudo docker cp /home/barakc82/app option-trader:/home/option-trader/ && sudo docker cp /home/barakc82/app_async option-trader:/home/option-trader/ && sudo docker cp /home/barakc82/utilities option-trader:/home/option-trader/ && sudo docker exec option-trader pkill -f 'python3 -m app_async.main' || true"
+
+echo Cleaning up...
+if exist scripts.tar.gz del scripts.tar.gz
+
+echo Update complete. The supervisor should restart the app shortly.
